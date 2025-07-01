cdbd278012e3989c6e4635ab1b43b99c3f11b1d4
Tejaswini3-ckm <tejaswiniammu321@gmail.com>
2025-07-01 06:02:49 +0530

    adding

---
+++ Earth.java
@@ -0,0 +1,14 @@
+class Earth{
+
+static String continents [] = {"Asia", "Africa", "North America", "South America", "Antarctica", "Europe", "Australia"};
+
+
+          public static void main(String args[]){
+
+   for(String continent:continents){
+	   System.out.println(continent);
+   }
+
+}
+
+}
